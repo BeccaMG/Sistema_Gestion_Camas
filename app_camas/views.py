939f637cc4933dca5a882bde29a575e3a667e084
leo7r <ramosleo12@gmail.com>
2014-03-18 22:42:31 -0430

    Haciendo listado de solicitudes, todavia por terminar

--- app_camas/views.py
+++ app_camas/views.py
@@ -12,12 +12,15 @@ from django.shortcuts import render_to_response,redirect,get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
 from models import *
+from app_solicitudes.models import *
 
 # Create your views here.
 @login_required(login_url='/')
 def asignar_habitacion(request):
-    habitaciones_libres = Habitacion.objects.filter(libre=True)
-    titulo = "Asignacion de Habitaciones"
-    info = {'habs':habitaciones_libres,
-            'titulo':titulo}
-    return render_to_response('asignar_habitacion.html',info,context_instance=RequestContext(request))
+	
+	habitaciones_libres = Habitacion.objects.filter(libre=True)
+	pacientes_emergencia_adultos = Solicitud.objects.filter(proveniencia=3)
+	
+	titulo = "Asignacion de Habitaciones"
+	info = {'habs':habitaciones_libres,'pac_emergencia_adultos':pacientes_emergencia_adultos,'titulo':titulo}
+	return render_to_response('asignar_habitacion.html',info,context_instance=RequestContext(request))
--- recursos/configuracion/settings.py
+++ recursos/configuracion/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'NAME': 'cmsb', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'le0x1212',
         'HOST': 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '', # Set to empty string for default.
     }
