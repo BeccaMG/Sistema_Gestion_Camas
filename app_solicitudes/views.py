5b706d48c877f3b332f3b892ed7f665385fe1d6c
Ronier <rronier@gmail.com>
2014-03-21 20:10:21 -0430

    Solicitudes, debugueado inicialmente
    Form solicitudes ya visible

--- app_solicitudes/views.py
+++ app_solicitudes/views.py
@@ -12,7 +12,10 @@ from django.forms.widgets import CheckboxSelectMultiple
 from django.shortcuts import render_to_response,redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
+# Manejo de Informacion de esta aplicacion
+##from forms import *
 from models import *
+from forms import *
 
 # Create your views here.
 def solicitar_habitacion(request):
@@ -25,30 +28,11 @@ def solicitar_habitacion(request):
             u_nombres          = pcd['nombres']
             u_apellidos        = pcd['apellidos']
             u_mun_historia     = pcd['num_historia']
-			prueba = Usuario.objects.filter(cedula=u_cedula)
-            prueba2 = (u_clave==u_clave0)
-            if not prueba:
-                if prueba2:
-                    prueba2 = (u_email==u_email0)
-                    if prueba2:
-		              u = Usuario(username=u_cedula,cedula=u_cedula,first_name=u_nombres,habilitado=True,last_name=u_apellidos,tipo=u_tipo,administrador=u_administrador,sexo=u_sexo,tlf_cel=u_cel,direccion=u_direccion,tlf_casa=u_tlf_casa,email=u_email,password=u_clave)
-		              u.is_active = True
-		              u.set_password(u_clave)
-		              if u_administrador == True:
-		                  u.is_staff = True
-		              u.save() 	
-		              return redirect('/')
-                    else:
-		            	msj_info = "No hubo coincidencia con los emails ingresados."     
-                else:
-                    msj_info = "No hubo coincidencia con las claves ingresadas."     
-            else:
-                msj_info = "Ya hay un usuario registrado con esa cedula."     
         else:
         	msj_info = "Error con el formulario."
         msj_tipo = "error"
         info = {'msj_tipo':msj_tipo,'msj_info':msj_info,'form':form}
-        return render_to_response('crearUsuario.html',info,context_instance=RequestContext(request))
+        return render_to_response('solicitar_habitacion.html',info,context_instance=RequestContext(request))
     form = SolicitarHabitacion()
     info = {'form':form}
-    return render_to_response('solicitarHabitacion.html',info,context_instance=RequestContext(request))
+    return render_to_response('solicitar_habitacion.html',info,context_instance=RequestContext(request))
