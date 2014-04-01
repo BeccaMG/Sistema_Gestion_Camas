f9a27006c37bdccbcce8dc8cea7b8535507d9785
leo7r <ramosleo12@gmail.com>
2014-03-31 20:45:18 -0430

    Sale la manito cuando agarras a un paciente y sirve el arrastrar y soltar en todos los tipos de pacientes

--- app_camas/views.py
+++ app_camas/views.py
@@ -20,31 +20,35 @@ from datetime import date
 @login_required(login_url='/')
 def asignar_habitacion(request):
 	
-	habitaciones_libres = Habitacion.objects.filter(libre=True)
-	pacientes_quirofano = Solicitud.objects.filter(procedencia=1)
-	pacientes_emergencia_pediatra = Solicitud.objects.filter(procedencia=2)
-	pacientes_emergencia_adultos = Solicitud.objects.filter(procedencia=3)
-	pacientes_parto = Solicitud.objects.filter(procedencia=4)
-	pacientes_uci = Solicitud.objects.filter(procedencia=5)
-	pacientes_adicional = Solicitud.objects.filter(procedencia=6)
-	pacientes_especial = Solicitud.objects.filter(procedencia=7)
-	pacientes_traslado = Solicitud.objects.filter(procedencia=8)
-	pacientes_otros = Solicitud.objects.filter(procedencia=9)
-	
-	titulo = "Asignacion de Habitaciones"
-	info = {
-	'habs':habitaciones_libres,
-	'pac_quirofano':pacientes_quirofano,
-	'pac_emergencia_adultos':pacientes_emergencia_adultos,
-	'pac_emergencia_pediatra':pacientes_emergencia_pediatra,
-	'pac_parto':pacientes_parto,
-	'pac_uci':pacientes_uci,
-	'pac_adicional':pacientes_adicional,
-	'pac_especial':pacientes_especial,
-	'pac_traslado':pacientes_traslado,
-	'pac_otros':pacientes_otros,
-	'titulo':titulo}
-	return render_to_response('asignar_habitacion.html',info,context_instance=RequestContext(request))
+	if request.method == 'POST':
+		print request.POST
+		return 'asd'
+	else:
+		habitaciones_libres = Habitacion.objects.filter(libre=True)
+		pacientes_quirofano = Solicitud.objects.filter(procedencia=1)
+		pacientes_emergencia_pediatra = Solicitud.objects.filter(procedencia=2)
+		pacientes_emergencia_adultos = Solicitud.objects.filter(procedencia=3)
+		pacientes_parto = Solicitud.objects.filter(procedencia=4)
+		pacientes_uci = Solicitud.objects.filter(procedencia=5)
+		pacientes_adicional = Solicitud.objects.filter(procedencia=6)
+		pacientes_especial = Solicitud.objects.filter(procedencia=7)
+		pacientes_traslado = Solicitud.objects.filter(procedencia=8)
+		pacientes_otros = Solicitud.objects.filter(procedencia=9)
+		
+		titulo = "Asignacion de Habitaciones"
+		info = {
+		'habs':habitaciones_libres,
+		'pac_quirofano':pacientes_quirofano,
+		'pac_emergencia_adultos':pacientes_emergencia_adultos,
+		'pac_emergencia_pediatra':pacientes_emergencia_pediatra,
+		'pac_parto':pacientes_parto,
+		'pac_uci':pacientes_uci,
+		'pac_adicional':pacientes_adicional,
+		'pac_especial':pacientes_especial,
+		'pac_traslado':pacientes_traslado,
+		'pac_otros':pacientes_otros,
+		'titulo':titulo}
+		return render_to_response('asignar_habitacion.html',info,context_instance=RequestContext(request))
 
 @login_required(login_url='/')
 def censo(request):
