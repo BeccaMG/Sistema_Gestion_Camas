eda50482736f12dde59d7bed6be44e955ea7ac0d
TroiKM <karentroiano2009@gmail.com>
2014-04-12 16:03:41 -0430

    Solicitud camillas lista.
    Se solicita exitosamente la camilla. Se piensa en cómo mejorar un
    detalle en la base de datos.

--- app_solicitudes/urls.py
+++ app_solicitudes/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url, include
 from views import *
 
 urlpatterns = patterns('app_solicitudes.views',
-
     # Acesso a esperas
     url('^habitacion/solicitar$','solicitar_habitacion'),
 )
--- app_solicitudes/views.py
+++ app_solicitudes/views.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 ##from django.shortcuts import render
 # Manejo de Sesion
 from django.contrib.auth import authenticate, login, logout
@@ -24,6 +26,7 @@ from forms import *
 # .
 # hora_en_segundos[6] -> Segundos en 6 horas
 hora_en_segundos = [0,3600,7200,10800,14400,18000,21600] 
+
 
 @login_required(login_url='/')
 def solicitar_habitacion(request):
@@ -53,25 +56,31 @@ def solicitar_habitacion(request):
                 newPaciente = Paciente.objects.get(cedula = s_cedula)
                 try:
                     newMedico = Medico.objects.get(cedula = s_doctor_cedula)
-                    s = Solicitud(paciente = newPaciente,
-                            num_historia = s_num_historia,
-                            diagnostico = s_diagnostico,
-                            medico = newMedico,
-                            fecha = s_fecha,
-                            fecha_salida = s_fecha_salida,
-                            procedencia = s_procedencia,
-                            correo = s_correo_solicitante,
-                            observacion = s_observacion)
-                    s.save()
-                    return redirect('/')
-                except:
+                    if (s_fecha <= s_fecha_salida):
+                        s = Solicitud(paciente = newPaciente,
+                                num_historia = s_num_historia,
+                                diagnostico = s_diagnostico,
+                                medico = newMedico,
+                                fecha = s_fecha,
+                                fecha_salida = s_fecha_salida,
+                                procedencia = s_procedencia,
+                                correo = s_correo_solicitante,
+                                observacion = s_observacion)
+                        s.save()
+                        return redirect('/')
+                    else:
+                        mnj_fecha = "La fecha de ingreso debe ser menor que la de egreso."
+                except ObjectDoesNotExist:
                     msj_tipo = "error"
                     msj_info = "El medico no se encuentra registrado en el sistema."
-            except:
+            except ObjectDoesNotExist:
                 msj_tipo = "error"
                 msj_info = "El paciente no se encuentra registrado en el sistema."
+            except:
+                msj_tipo = "error"
+                msj_info = "Ya existe una solicitud con este paciente."
             
-        info = {'msj_tipo':msj_tipo,'msj_info':msj_info,'form':form}
+        info = {'msj_tipo':msj_tipo,'msj_info':msj_info,'form':form, 'mnj_fecha':mnj_fecha}
         return render_to_response('solicitar_habitacion.html',info,context_instance=RequestContext(request))
     form = SolicitarHabitacion()
     info = {'form':form}
