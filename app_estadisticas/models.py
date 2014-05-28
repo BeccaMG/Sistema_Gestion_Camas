65bf770534339cf45273c0c641ec710b9c47b4f9
TroiKM <karentroiano2009@gmail.com>
2014-05-28 11:01:33 -0430

    Termometro/Matriz

--- app_estadisticas/models.py
+++ app_estadisticas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from app_solicitudes.models import *
+from app_camas.models import *
+from datetime import date
 
-# Create your models here.
+class Termometro(models.Model):
+
+	fecha_termometro = models.DateField()
+	numero = models.CharField(max_length=6, unique=True)
+	fecha_ingreso = models.DateField()
