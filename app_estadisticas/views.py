c11d1faf55854e740a1fdcc7ac88962549816cf4
TroiKM <karentroiano2009@gmail.com>
2014-05-27 23:47:38 -0430

    Termometro más cooperante
    blah

--- app_estadisticas/views.py
+++ app_estadisticas/views.py
@@ -12,22 +12,22 @@ from app_solicitudes.models import *
 from app_camas.models import *
 import datetime
 from django.utils import timezone
-import pdb
+
 from django.utils import simplejson
 
 # Termometro = {'fecha':camas}
-camas_prueba = Habitacion.objects.all().filter(estado='D')
-camas_prueba1 = Habitacion.objects.all().filter(estado='O')
-camas = [camas_prueba, camas_prueba1]
-Termometro = {'2014-05-24':camas, '2014-05-25':camas}
+camas_libres = Habitacion.objects.all().filter(estado='D')
+camas_verdes = Ingreso.objects.all().filter(habitacion__estado='O') 
+camas_amarillas = Ingreso.objects.all().filter(habitacion__estado='O') 
+camas_rojas = Ingreso.objects.all().filter(habitacion__estado='O') 
+camas = [camas_libres, camas_verdes, camas_amarillas, camas_rojas]
+Termometro = {'2014-05-26':camas}
 
 @login_required(login_url='/')
 def termometro(request):
     
     now = timezone.now()
     nowS = str(now.date())
-    
-    #pdb.set_trace()
     
     if (nowS > Termometro.keys()[-1]):
     
@@ -43,25 +43,17 @@ def termometro(request):
         camas_verdes = Ingreso.objects.all().filter(habitacion__estado='O', fecha_ingreso__gte=dos_diasS,fecha_ingreso__lte=nowS)  
         camas_amarillas = Ingreso.objects.all().filter(habitacion__estado='O', fecha_ingreso__gte=cuatro_diasS,fecha_ingreso__lte=dos_diasS)  
         camas_rojas = Ingreso.objects.all().filter(habitacion__estado='O', fecha_ingreso__lt=cuatro_diasS)  
-        
-        ingresos = Ingreso.objects.all()
-        hoy = date.today
-        
-        #pdb.set_trace()
+         
         Termometro[nowS] = [camas_libres, camas_verdes, camas_amarillas, camas_rojas]
         
         info = {
-            'hoy':hoy,
-            'ingresos':ingresos,
             'Termometro':Termometro,    
         }
     else:
-        ingresos = Ingreso.objects.all()
+	
         hoy = date.today
         info = {
             'Termometro':Termometro,
-            'ingresos':ingresos,
-            'hoy':hoy,
         }
         
     return render_to_response('estadistica_termometro.html',info,context_instance=RequestContext(request))
